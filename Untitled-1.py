e22e9c90233f87e42ba7b8958846dac5303e1f5b
uytyftyfiyuifuyfu <146858800+uytyftyfiyuifuyfu@users.noreply.github.com>
2023-10-05 18:13:08 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+from pygame import *
+back =  (200,255,255)
+win_width  = 600
+win_height = 500
+window = display.set_mode((win_width,win_height))                          
+window.fill(back)
+clock = time.Clock()
+FPS = 120
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
